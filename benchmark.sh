cd8fb1f3877742f4f600b8b354c9107b8cd0c8df
Pedro <pedrogomesyoo@gmail.com>
2017-09-21 22:40:11 +0200

    small changes to bench script

--- benchmark.sh
+++ benchmark.sh
@@ -2,8 +2,8 @@
 # Run benchmark test at multiple levels of concurrency
 # and compare results
 
-FILE_LOG_URL="http://localhost/?driver=fs&nr_tasks=4"
-REDIS_LOG_URL="http://localhost/?driver=redis&nr_tasks=4"
+FILE_LOG_URL="http://127.0.0.1/?driver=fs&nr_tasks=4"
+REDIS_LOG_URL="http://127.0.0.1/?driver=redis&nr_tasks=4"
 REQUESTS_COUNT=10000
 
 function print_difference
@@ -12,34 +12,37 @@ function print_difference
   echo $DIFFERENCE% - redis: $1 file: $2
 }
 
+
 echo "## TEST 1 ##"
 CONCURRENCY=50
 echo "Concurrency: $CONCURRENCY "
-JOB_REDIS=$(ab -n $REQUESTS_COUNT -c $CONCURRENCY -q "$REDIS_LOG_URL"  | grep 'Requests per second' | awk '{print $4}')
-JOB_FILE=$(ab -n $REQUESTS_COUNT -c $CONCURRENCY -q "$FILE_LOG_URL"  | grep 'Requests per second' | awk '{print $4}')
+JOB_REDIS=$(ab -r -n $REQUESTS_COUNT -c $CONCURRENCY -q "$REDIS_LOG_URL"  | grep 'Requests per second' | awk '{print $4}')
+JOB_FILE=$(ab -r -n $REQUESTS_COUNT -c $CONCURRENCY -q "$FILE_LOG_URL"  | grep 'Requests per second' | awk '{print $4}')
 print_difference $JOB_REDIS $JOB_FILE
 echo
 
 echo "## TEST 2 ##"
-CONCURRENCY=200
+CONCURRENCY=150
 echo "Concurrency: $CONCURRENCY "
-JOB_REDIS=$(ab -n $REQUESTS_COUNT -c $CONCURRENCY -q "$REDIS_LOG_URL"  | grep 'Requests per second' | awk '{print $4}')
-JOB_FILE=$(ab -n $REQUESTS_COUNT -c $CONCURRENCY -q "$FILE_LOG_URL"  | grep 'Requests per second' | awk '{print $4}')
+JOB_REDIS=$(ab -r -n $REQUESTS_COUNT -c $CONCURRENCY -q "$REDIS_LOG_URL"  | grep 'Requests per second' | awk '{print $4}')
+JOB_FILE=$(ab -r -n $REQUESTS_COUNT -c $CONCURRENCY -q "$FILE_LOG_URL"  | grep 'Requests per second' | awk '{print $4}')
 print_difference $JOB_REDIS $JOB_FILE
 echo
 
+REQUESTS_COUNT=30000
+
 echo "## TEST 3 ##"
-CONCURRENCY=400
+CONCURRENCY=300
 echo "Concurrency: $CONCURRENCY "
-JOB_REDIS=$(ab -n $REQUESTS_COUNT -c $CONCURRENCY -q "$REDIS_LOG_URL"  | grep 'Requests per second' | awk '{print $4}')
-JOB_FILE=$(ab -n $REQUESTS_COUNT -c $CONCURRENCY -q "$FILE_LOG_URL"  | grep 'Requests per second' | awk '{print $4}')
+JOB_REDIS=$(ab -r -n $REQUESTS_COUNT -c $CONCURRENCY -q "$REDIS_LOG_URL"  | grep 'Requests per second' | awk '{print $4}')
+JOB_FILE=$(ab -r -n $REQUESTS_COUNT -c $CONCURRENCY -q "$FILE_LOG_URL"  | grep 'Requests per second' | awk '{print $4}')
 print_difference $JOB_REDIS $JOB_FILE
 echo
 
 echo "## TEST 4 ##"
 CONCURRENCY=600
 echo "Concurrency: $CONCURRENCY "
-JOB_REDIS=$(ab -n $REQUESTS_COUNT -c $CONCURRENCY -q "$REDIS_LOG_URL"  | grep 'Requests per second' | awk '{print $4}')
-JOB_FILE=$(ab -n $REQUESTS_COUNT -c $CONCURRENCY -q "$FILE_LOG_URL"  | grep 'Requests per second' | awk '{print $4}')
+JOB_REDIS=$(ab -r -n $REQUESTS_COUNT -c $CONCURRENCY -q "$REDIS_LOG_URL"  | grep 'Requests per second' | awk '{print $4}')
+JOB_FILE=$(ab -r -n $REQUESTS_COUNT -c $CONCURRENCY -q "$FILE_LOG_URL"  | grep 'Requests per second' | awk '{print $4}')
 print_difference $JOB_REDIS $JOB_FILE
 echo
